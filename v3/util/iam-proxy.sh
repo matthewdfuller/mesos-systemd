c4c2488364c3182f44b9f57d3907927cd976bc84
Matt Fuller <fuller@adobe.com>
2016-03-24 14:21:38 -0400

    moving iam proxy setup script to util

--- v3/util/iam-proxy.sh
+++ v3/util/iam-proxy.sh
@@ -10,6 +10,56 @@ if [ "${NODE_ROLE}" != "worker" ]; then
     exit 0
 fi
 
+# First, setup a service to run the proxy
+
+mkdir -p /etc/services
+IAM_PROXY_SERVICE_FILE=/etc/services/iam-proxy@.service
+
+cat > $IAM_PROXY_SERVICE_FILE <<EOF
+[Unit]
+Description=IAMProxy @ %i
+After=docker.service bootstrap.service
+Requires=docker.service bootstrap.service
+
+[Service]
+Environment="IMAGE=etcdctl get /images/iam-proxy"
+EnvironmentFile=/etc/environment
+
+User=core
+Restart=always
+RestartSec=20
+TimeoutStartSec=0
+
+ExecStartPre=/usr/bin/sh -c "docker pull $($IMAGE)"
+ExecStartPre=-/usr/bin/docker kill iam_proxy
+ExecStartPre=-/usr/bin/docker rm iam_proxy
+
+ExecStart=/usr/bin/sh -c "/usr/bin/docker run \
+  --name=iam_proxy \
+  --volume /var/run/docker.sock:/var/run/docker.sock \
+  --restart=always \
+  --net=host \
+  $($IMAGE)
+
+ExecStop=-/usr/bin/docker stop iam_proxy
+
+[Install]
+WantedBy=multi-user.target
+
+[X-Fleet]
+Global=false
+MachineMetadata=role=worker
+MachineMetadata=ip=%i
+EOF
+
+chown root:root $IAM_PROXY_SERVICE_FILE
+chmod 0644 $IAM_PROXY_SERVICE_FILE
+
+sudo fleetctl submit "${IAM_PROXY_SERVICE_FILE}"
+sudo fleetctl start "${iam-proxy%.service}${COREOS_PRIVATE_IPV4}"
+
+# Next, update IPTABLES to route IAM requests to the newly launched service
+
 export NETWORK="bridge"
 export GATEWAY="$(ifconfig docker0 | grep "inet " | awk -F: '{print $1}' | awk '{print $2}')"
 export INTERFACE="docker0"
